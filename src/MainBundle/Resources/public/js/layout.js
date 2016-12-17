7730e738f8956bec43ccdf16c8844672ae1c9429
Maky <Maky@DESKTOP-0ARH2U3.home>
2016-12-17 15:32:28 +0100

    copy design from Manaceo

---
+++ src/MainBundle/Resources/public/js/layout.js
@@ -0,0 +1,7 @@
+function form() {
+    $('input').attr("class", "form-control");
+    $('label').attr("class", "control-label");
+    $('textarea').attr("class", "form-control");
+    $('select').attr("class", "form-control");
+    $('#btn').attr("class", "btn btn-primary");
+}
