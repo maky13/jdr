311c44afcc84d4838b497706d10a97f148ab5371
Maky13 <maky13@hotmail.fr>
2016-12-25 14:50:46 +0100

    init Naheulbeuk bundle

---
+++ src/NaheulbeukBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NaheulbeukBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('NaheulbeukBundle:Default:index.html.twig');
+    }
+}
